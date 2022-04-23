0fd75a275683fab5b93fbcfa7e5b79bd82acd83b
Ruank6 <34999556+Ruank6@users.noreply.github.com>
2022-04-23 15:09:27 +0100

    Filter the Table Data

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # list all food customers can pick
-streamlit.multiselect("Pick some fruit: ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruit: ", list(my_fruit_list.index),['Avocado','Strawberries'])
 
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display table on page
 streamlit.dataframe(my_fruit_list)
